3c81f317c6a7ce594e1aa0ff17d1fbe5b3162b72
mara-olson <mara.c.olson@gmail.com>
2022-04-27 19:13:48 -0700

    Functional cards with three hard-coded books, their titles, date read, and rating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import Books from "./components/Books";
+
 function App() {
+  const library = [
+    {
+      id: "1",
+      title: "The Meaning of Man",
+      author: "Viktor Frankl",
+      read_date: new Date(2020, 7, 14),
+      rate: "4",
+    },
+    {
+      id: "2",
+      title: "Sapiens",
+      author: "Yuval Harari",
+      read_date: new Date(2021, 2, 28),
+      rate: "5",
+    },
+    {
+      id: "3",
+      title: "Zen and the Art of Motorcycle Maintenance",
+      author: "Rovert Pirsig",
+      read_date: new Date(2021, 5, 12),
+      rate: "3",
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h1>My Library</h1>
+      <Books library={library} />
     </div>
   );
 }
